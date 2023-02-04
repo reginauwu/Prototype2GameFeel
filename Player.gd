463084bbd5e50cb07ba0ced4202ff5932d8051e9
reginauwu <wregina0418@gmail.com>
2023-02-04 12:46:10 -0500

    kirby mechanisms
    run and jump

---
+++ Player.gd
@@ -0,0 +1,54 @@
+extends KinematicBody2D
+
+const up = Vector2(0, -1)
+const grav = 100
+const maxFallSpeed = 800
+const maxSpeed = 500#80
+const jumpForce = 1500#300 
+const accel = 50#10 
+
+var motion = Vector2()
+var dirRight = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(_delta):
+	motion.y += grav 
+	if motion.y > maxFallSpeed:
+		motion.y = maxFallSpeed
+	
+	if dirRight == true:
+		$Sprite.scale.x = 1
+	else:
+		$Sprite.scale.x = -1
+	
+	motion.x = clamp(motion.x, -maxSpeed, maxSpeed)
+	
+	if Input.is_action_pressed("right"):
+		motion.x += accel
+		dirRight = true
+		$AnimationPlayer.play("run")
+	elif Input.is_action_pressed("left"):
+		motion.x += -accel
+		dirRight = false 
+		$AnimationPlayer.play("run")
+	else:
+		motion.x = lerp(motion.x, 0, 0.2)
+		$AnimationPlayer.play("idle")
+		
+	if is_on_floor():
+		if Input.is_action_pressed("jump"):
+			motion.y = -jumpForce
+			
+	if !is_on_floor():
+		#if motion.y < 0:
+			#$AnimationPlayer.play("jump")
+		#elif motion.y > 0:
+			#$AnimationPlayer.play("run") #fall
+		#else:
+			#$AnimationPlayer.play("jump")
+		$AnimationPlayer.play("jump")
+			
+	motion = move_and_slide(motion, up)
